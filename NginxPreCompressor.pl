56bfd947afc8bba8295ce105368971f8531708cb
J-F <ever.livin.never.fearful@gmail.com>
2013-08-21 23:15:12 -0400

    First commit

---
+++ NginxPreCompressor.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+####
+# This is a helper script to pre compress file when using the Nginx 
+# HttpGzipStaticModuleavailable at http://wiki.nginx.org/HttpGzipStaticModule
+# Full source and documentation is available at http://github.com/DontBelieveTheByte/HttpGzipStaticPreCompressor
+# Published under the GPLv3 license or later versions.
+# Full license is available here : https://www.gnu.org/licenses/gpl.html
+# (c) J-F B 2013
+####
+use Cwd;
+use strict;
+use warnings;
+
+#All the file extensions we'll be compressing
+my @extensions= ("css", "js", "svg", "otf", "ttf", "woff");
+
+#Uncomment to set a hard coded directory.
+my $customDir = '/usr/share/nginx/kbm/web/';
+
+#Make sure gzip and touch are available.
+if (!`which touch` || !`which gzip`){
+    die "Unable to find touch or gzip commands on your system.\n";
+}
+
+#Use hard coded directory or command line argument as a starting point.
+my $startDir ;
+if (defined $customDir) {
+    $startDir = $customDir;
+}
+if (defined $ARGV[1]){
+    $startDir = $ARGV[1];
+}
+if (!defined $startDir || !-d $startDir) {
+    die "Input directory error.\n"
+}
+#Remove trailing slash from starting directory if present.
+$startDir =~ s/(.+)\/$/$1/;
+
+#Go to the directory we want to work with.
+chdir $startDir or die "Cannot go to starting directort";
+print "Starting to recusively compress files from : $startDir\n";
+
+#Fill the directory array.
+my @listOfDirs = `find -L ./ -type d`;
+push(@listOfDirs, $startDir);
+
+foreach my $currentDir (@listOfDirs){
+    #print "$currentDir\n";
+        my $fullPathCurrentDir;
+        unless ($currentDir ."/	" eq $startDir || $currentDir eq $startDir){
+	    $fullPathCurrentDir = "$startDir". substr ($currentDir,1);
+            print "$fullPathCurrentDir\n";
+            chomp $fullPathCurrentDir;
+            chdir $fullPathCurrentDir or die "Cannot change working directory to : \n$fullPathCurrentDir\n";
+        }
+        foreach my $exts(@extensions){
+            foreach my $file (<*.$exts>){
+            print "Compressing : $file\n";
+            system("gzip -9 -f -N < $file > $file.gz && touch -r $file $file.gz") == 0 or
+            die "Compression operation error at : \n" . $fullPathCurrentDir. "$file\n";
+            }
+        }	
+}
+exit(0);
---
+++ README.md
@@ -0,0 +1,26 @@
+#Ngninx Gzip Static Pre Compressor
+___
+## Purpose 
+
+This is a helper script to pre compress file when using the Nginx 
+HttpGzipStatic module available at http://wiki.nginx.org/HttpGzipStaticModule
+
+___
+##Usage
+
+1. Change the @extensions array to indicate which file extensions you want to compress.
+2. Set a hard coded directory path by changing the $customDir variable or just use a command line parameter when invoking the script.
+3. Run the script manually, when deploying/updating your app or set up a cron job to run it at regular intervals.
+
+___
+## Requirements
+
+- Any unix based system with gzip and touch commands installed
+
+- Perl 5
+___
+## License and copyright
+
+GPLv3 or later versions :  https://www.gnu.org/licenses/gpl.html
+
+Copyright (c) J-F B 2013
